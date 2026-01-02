3b14fb47f1d8868883e48c56234eefa927c15132
princewill-dev <ezekudep@gmail.com>
2026-01-02 13:32:50 +0100

    - removed scroll indicator

--- resources/views/storefront/layout.blade.php
+++ resources/views/storefront/layout.blade.php
@@ -53,11 +53,11 @@
       <!-- pre loader area end -->
 
       <!-- back to top start -->
-      <div class="progress-wrap">
+      <!-- <div class="progress-wrap">
          <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
             <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
          </svg>
-      </div>
+      </div> -->
       <!-- back to top end -->
 
       <!-- header area start -->
