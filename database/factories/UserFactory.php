2af238a92e376fed6c699332f5519f02dc9fb3ae
princewill-dev <ezekudep@gmail.com>
2026-06-02 14:48:33 +0100

    seeder fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,8 +25,8 @@ class UserFactory extends Factory
     {
         return [
             'uuid' => (string) Str::uuid(),
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
