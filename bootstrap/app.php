ed9e3339e3a9b22d5c44bb36f145ba8c23447145
princewill-dev <ezekudep@gmail.com>
2026-07-14 16:51:13 +0100

    - config: removed start.sh

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,23 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// --- ADD THIS BLOCK ---
+$storageDirs = [
+    __DIR__.'/../storage/app/public',
+    __DIR__.'/../storage/framework/cache/data',
+    __DIR__.'/../storage/framework/sessions',
+    __DIR__.'/../storage/framework/testing',
+    __DIR__.'/../storage/framework/views',
+    __DIR__.'/../storage/logs',
+];
+
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        @mkdir($dir, 0755, true);
+    }
+}
+// ----------------------
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
